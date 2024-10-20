168a1531b103b659b257106dcfdf470ebdd41ede
sruizlo2 <sruizlo2@eafit.edu.co>
2024-10-20 16:12:39 -0400

    Updated script to create spectral reflectance

--- scripts/CreateDBR.py
+++ scripts/CreateDBR.py
@@ -4,16 +4,19 @@ Created on Sat Oct 19 18:56:11 2024
 
 @author: user
 """
+import numpy as np
 
-def Create_DBR(n_pairs, index_in, index_1, index_2, thickness, index_out=1):
+def Create_DBR(n_pairs, index_in, index_1, index_2, thickness, precision=0.05, index_out=1):
     # Entry medium
     layers = [(index_in, 0)]
     # Alteranting layers
-    for i in range(n_pairs):
-        layer_1 = (index_1, thickness)
-        layer_2 = (index_2, thickness)
+    for i in range(int(np.ceil(n_pairs))):
+        layer_1 = (index_1, thickness * (1 + (precision * (2 * np.random.rand() - 1))))
+        layer_2 = (index_2, thickness * (1 + (precision * (2 * np.random.rand() - 1))))
         layers.append(layer_1)
-        layers.append(layer_2)
+        if i <= n_pairs - 1:
+            layers.append(layer_2)
+            
     # Exit medium
     layers.append((index_out, 0))
     
---
+++ scripts/TTM_Ideal_DBR_Optimal_Design_Spectra.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 16:00:26 2024
+
+@author: srulo
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from TTM import Transfer_matrix_system as TM
+from TTM import Trans_and_reflec as TR
+from MatFileLoader import Read_mat_file as ReadMatFile
+from CreateDBR import Create_DBR as DBR
+import os
+
+# Define spectrum
+wavelength_design = 850e-9; # meters
+wavelength_range = 400e-9; # meters
+wavelegnth_step = 1e-9; # meters
+# Vector with testing wavelengths
+wavelengths = np.arange(wavelength_design - wavelength_range / 2,
+                        wavelength_design + wavelength_range / 2, wavelegnth_step)
+wavelength_design_index = np.argwhere(abs(wavelengths - wavelength_design) < 1e-16)[0][0]
+
+# Path to materials files
+path_to_mat_files = '../materials'
+mat_file_GaAs = 'GaAs'
+mat_file_AlGaAs = 'AlGaAs-X=0.219'
+mat_file_AlAs = 'AlAs'
+
+# Design parameters
+target_reflectance_top = 0.995;
+target_reflectance_bottom = 0.95;
+# Materials parameters, n and K for design wavelegnth
+#index_Mat_GaAs_design, loss_Mat_GaAs_design = ReadMatFile(wavelength_design, os.path.join(path_to_mat_files, mat_file_GaAs))
+#index_Mat_AlGaAs_design, loss_Mat_AlGaAs_design = ReadMatFile(wavelength_design, os.path.join(path_to_mat_files, mat_file_AlGaAs))
+#index_Mat_AlAs_design, loss_Mat_AlAs_design = ReadMatFile(wavelength_design, os.path.join(path_to_mat_files, mat_file_AlAs))
+
+# Load materials parameters, n and K for all wavelength
+index_Mat_GaAs, loss_Mat_GaAs = ReadMatFile(wavelengths, os.path.join(path_to_mat_files, mat_file_GaAs), False)
+index_Mat_AlGaAs, loss_Mat_AlGaAs = ReadMatFile(wavelengths, os.path.join(path_to_mat_files, mat_file_AlGaAs))
+index_Mat_AlAs, loss_Mat_AlAs = ReadMatFile(wavelengths, os.path.join(path_to_mat_files, mat_file_AlAs))
+
+# Plot spectral reflectance for this design
+#plt.figure()
+#plt.plot(wavelengths,index_Mat_1,label='n')
+#plt.plot(wavelengths,index_Mat_2,label='R_TM')
+#plt.show
+
+### TOP MIRROR
+# Vector with testing number of layer pairs
+n_pairs = np.array([23])
+# Set indices of refraction for input layer
+index_Mat_in = index_Mat_AlGaAs
+loss_Mat_in = loss_Mat_AlGaAs
+# Set indices of refraction for alternating layers
+index_Mat_1 = index_Mat_AlAs
+loss_Mat_1 = loss_Mat_AlAs
+index_Mat_2 = index_Mat_AlGaAs
+loss_Mat_2 = loss_Mat_AlGaAs
+# Set indices of refraction for output layer
+index_Mat_out = np.ones(wavelengths.shape[0]) # Air
+loss_Mat_out = np.zeros(wavelengths.shape[0]) # Air
+
+# Set parameters for design wavelength
+index_Mat_1_design = index_Mat_1[wavelength_design_index]
+loss_Mat_1_design = loss_Mat_1[wavelength_design_index]
+index_Mat_2_design = index_Mat_2[wavelength_design_index]
+loss_Mat_2_design = loss_Mat_2[wavelength_design_index]
+
+# Average index of refraction
+average_index = (index_Mat_1_design + index_Mat_2_design) / 2
+thickness_ideal = wavelength_design / 4 / average_index # lambda / 4 condition (per layer)
+# Normal incidence
+angle_inc = 0
+# Number of runs to emulate non-ideal thicknesses
+num_runs = 1
+
+# Iterate over design configurations
+# Initialize variables
+DBR_TE_trans = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TE_reflect = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TM_trans = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TM_reflect = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TE_mean_trans_design = []
+DBR_TE_mean_reflect_design = []
+DBR_TM_mean_trans_design = []
+DBR_TM_mean_reflect_design = []
+DBR_TE_std_trans_design = []
+DBR_TE_std_reflect_design = []
+DBR_TM_std_trans_design = []
+DBR_TM_std_reflect_design = []
+
+n_pair_index = 0;
+for n_pair in n_pairs:
+    wavelength_index = 0;
+    for wavelength in wavelengths:
+        # Assing index of refractions and losses for this wavelength
+        index_in = index_Mat_in[wavelength_index] + 1j * loss_Mat_in[wavelength_index]
+        index_1 = index_Mat_1[wavelength_index] + 1j * loss_Mat_1[wavelength_index]
+        index_2 = index_Mat_2[wavelength_index] + 1j * loss_Mat_2[wavelength_index]
+        index_out = index_Mat_out[wavelength_index] + 1j * loss_Mat_out[wavelength_index]
+        # Create DBR
+        DBR_TE_trans_run = []
+        DBR_TE_reflect_run = []
+        DBR_TM_trans_run = []
+        DBR_TM_reflect_run = []
+        for run in range(num_runs):
+            DBR_ = DBR(n_pair, index_in, index_1, index_2, thickness_ideal, 0.0, index_out)
+            # Transfer matrix (wavelegnth in meters!)
+            transfer_matrix_TE_DBR, transfer_matrix_TM_DBR = TM(DBR_, wavelength, angle_inc)
+            # Transmittance and Reflectance
+            DBR_TE_trans_, DBR_TE_reflect_ = TR(transfer_matrix_TE_DBR)
+            DBR_TM_trans_, DBR_TM_reflect_ = TR(transfer_matrix_TM_DBR)
+            DBR_TE_trans_run.append(DBR_TE_trans_)
+            DBR_TE_reflect_run.append(DBR_TE_reflect_)
+            DBR_TM_trans_run.append(DBR_TM_trans_)
+            DBR_TM_reflect_run.append(DBR_TM_reflect_)
+            
+        # Mean values of runs
+        DBR_TE_trans[n_pair_index, wavelength_index, 0] = np.mean(DBR_TE_trans_run)
+        DBR_TE_reflect[n_pair_index, wavelength_index, 0] = np.mean(DBR_TE_reflect_run)
+        DBR_TM_trans[n_pair_index, wavelength_index, 0] = np.mean(DBR_TM_trans_run)
+        DBR_TM_reflect[n_pair_index, wavelength_index, 0] = np.mean(DBR_TM_reflect_run)
+        # Standard deviation over runs
+        DBR_TE_trans[n_pair_index, wavelength_index, 1] = np.std(DBR_TE_trans_run)
+        DBR_TE_reflect[n_pair_index, wavelength_index, 1] = np.std(DBR_TE_reflect_run)
+        DBR_TM_trans[n_pair_index, wavelength_index, 1] = np.std(DBR_TM_trans_run)
+        DBR_TM_reflect[n_pair_index, wavelength_index, 1] = np.std(DBR_TM_reflect_run)
+        # Next wavelength
+        wavelength_index += 1
+    
+    # Assign reflectance for design wavelength
+    DBR_TE_mean_trans_design.append(DBR_TE_trans[n_pair_index, wavelength_design_index, 0])
+    DBR_TE_mean_reflect_design.append(DBR_TE_reflect[n_pair_index, wavelength_design_index, 0])
+    DBR_TM_mean_trans_design.append(DBR_TM_trans[n_pair_index, wavelength_design_index, 0])
+    DBR_TM_mean_reflect_design.append(DBR_TM_reflect[n_pair_index, wavelength_design_index, 0])
+    DBR_TE_std_trans_design.append(DBR_TE_trans[n_pair_index, wavelength_design_index, 1])
+    DBR_TE_std_reflect_design.append(DBR_TE_reflect[n_pair_index, wavelength_design_index, 1])
+    DBR_TM_std_trans_design.append(DBR_TM_trans[n_pair_index, wavelength_design_index, 1])
+    DBR_TM_std_reflect_design.append(DBR_TM_reflect[n_pair_index, wavelength_design_index, 1])
+    
+    # Plot spectral reflectance for this design
+    show_spectral_reflectance = True
+    if show_spectral_reflectance:
+        plt.figure()
+        plt.plot(wavelengths*1e9, DBR_TE_reflect[n_pair_index, :],'k',label='R')
+        plt.plot(wavelengths*1e9, DBR_TE_trans[n_pair_index, :],'#87aadeff',label='T')
+        absorpt = 1 - DBR_TE_reflect[n_pair_index, :, 0] - DBR_TE_trans[n_pair_index, :, 0]
+        plt.plot(wavelengths*1e9, absorpt,'#ff5555ff',label='A')
+        plt.axvline(x = wavelength_design*1e9, linestyle = '--', color = 'k')
+        plt.axhline(y = target_reflectance_top, linestyle = '--', color = 'k')
+        # plt.axhline(y = target_reflectance_bottom, linestyle = '--', color = 'k', label = 'Bottom DBR Target reflectance')
+        plt.xlim(wavelengths[0]*1e9, wavelengths[-1]*1e9)
+        plt.ylim(0, 1.05)
+        plt.grid()
+        plt.xlabel('Wavelegnth (nm)')
+        plt.ylabel('Reflectance')
+        plt.title('Top DBR mirror')
+        plt.show()
+    
+    # Print this reflectance
+    this_DBR_TE_reflect = DBR_TE_mean_reflect_design[n_pair_index]
+    print(f"N layers: {n_pair} | Reflectance: {this_DBR_TE_reflect}")
+    # Next N
+    n_pair_index += 1
+
+# %%
+### TOP MIRROR
+# Vector with testing number of layer pairs
+n_pairs = np.array([14 + 0.5])
+# Set indices of refraction for input layer
+index_Mat_in = index_Mat_AlGaAs
+loss_Mat_in = loss_Mat_AlGaAs
+# Set indices of refraction for alternating layers
+index_Mat_1 = index_Mat_AlAs
+loss_Mat_1 = loss_Mat_AlAs
+index_Mat_2 = index_Mat_AlGaAs
+loss_Mat_2 = loss_Mat_AlGaAs
+# Set indices of refraction for output layer
+index_Mat_out = index_Mat_GaAs # Substrate
+loss_Mat_out = index_Mat_GaAs # Substrate
+
+# Set parameters for design wavelength
+index_Mat_1_design = index_Mat_1[wavelength_design_index]
+loss_Mat_1_design = loss_Mat_1[wavelength_design_index]
+index_Mat_2_design = index_Mat_2[wavelength_design_index]
+loss_Mat_2_design = loss_Mat_2[wavelength_design_index]
+
+# Average index of refraction
+average_index = (index_Mat_1_design + index_Mat_2_design) / 2
+thickness_ideal = wavelength_design / 4 / average_index # lambda / 4 condition (per layer)
+# Normal incidence
+angle_inc = 0
+# Number of runs to emulate non-ideal thicknesses
+num_runs = 1
+
+# Iterate over design configurations
+# Initialize variables
+DBR_TE_trans = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TE_reflect = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TM_trans = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TM_reflect = np.zeros((n_pairs.shape[0], wavelengths.shape[0], 2))
+DBR_TE_mean_trans_design = []
+DBR_TE_mean_reflect_design = []
+DBR_TM_mean_trans_design = []
+DBR_TM_mean_reflect_design = []
+DBR_TE_std_trans_design = []
+DBR_TE_std_reflect_design = []
+DBR_TM_std_trans_design = []
+DBR_TM_std_reflect_design = []
+
+n_pair_index = 0;
+for n_pair in n_pairs:
+    wavelength_index = 0;
+    for wavelength in wavelengths:
+        # Assing index of refractions and losses for this wavelength
+        index_in = index_Mat_in[wavelength_index] + 1j * loss_Mat_in[wavelength_index]
+        index_1 = index_Mat_1[wavelength_index] + 1j * loss_Mat_1[wavelength_index]
+        index_2 = index_Mat_2[wavelength_index] + 1j * loss_Mat_2[wavelength_index]
+        index_out = index_Mat_out[wavelength_index] + 1j * loss_Mat_out[wavelength_index]
+        # Create DBR
+        DBR_TE_trans_run = []
+        DBR_TE_reflect_run = []
+        DBR_TM_trans_run = []
+        DBR_TM_reflect_run = []
+        
+        for run in range(num_runs):
+            DBR_ = DBR(n_pair, index_in, index_1, index_2, thickness_ideal, 0.0, index_out)
+            # Transfer matrix (wavelegnth in meters!)
+            transfer_matrix_TE_DBR, transfer_matrix_TM_DBR = TM(DBR_, wavelength, angle_inc)
+            # Transmittance and Reflectance
+            DBR_TE_trans_, DBR_TE_reflect_ = TR(transfer_matrix_TE_DBR)
+            DBR_TM_trans_, DBR_TM_reflect_ = TR(transfer_matrix_TM_DBR)
+            DBR_TE_trans_run.append(DBR_TE_trans_)
+            DBR_TE_reflect_run.append(DBR_TE_reflect_)
+            DBR_TM_trans_run.append(DBR_TM_trans_)
+            DBR_TM_reflect_run.append(DBR_TM_reflect_)
+            
+        # Mean values of runs
+        DBR_TE_trans[n_pair_index, wavelength_index, 0] = np.mean(DBR_TE_trans_run)
+        DBR_TE_reflect[n_pair_index, wavelength_index, 0] = np.mean(DBR_TE_reflect_run)
+        DBR_TM_trans[n_pair_index, wavelength_index, 0] = np.mean(DBR_TM_trans_run)
+        DBR_TM_reflect[n_pair_index, wavelength_index, 0] = np.mean(DBR_TM_reflect_run)
+        # Standard deviation over runs
+        DBR_TE_trans[n_pair_index, wavelength_index, 1] = np.std(DBR_TE_trans_run)
+        DBR_TE_reflect[n_pair_index, wavelength_index, 1] = np.std(DBR_TE_reflect_run)
+        DBR_TM_trans[n_pair_index, wavelength_index, 1] = np.std(DBR_TM_trans_run)
+        DBR_TM_reflect[n_pair_index, wavelength_index, 1] = np.std(DBR_TM_reflect_run)
+        # Next wavelength
+        wavelength_index += 1
+    
+    # Assign reflectance for design wavelength
+    DBR_TE_mean_trans_design.append(DBR_TE_trans[n_pair_index, wavelength_design_index, 0])
+    DBR_TE_mean_reflect_design.append(DBR_TE_reflect[n_pair_index, wavelength_design_index, 0])
+    DBR_TM_mean_trans_design.append(DBR_TM_trans[n_pair_index, wavelength_design_index, 0])
+    DBR_TM_mean_reflect_design.append(DBR_TM_reflect[n_pair_index, wavelength_design_index, 0])
+    DBR_TE_std_trans_design.append(DBR_TE_trans[n_pair_index, wavelength_design_index, 1])
+    DBR_TE_std_reflect_design.append(DBR_TE_reflect[n_pair_index, wavelength_design_index, 1])
+    DBR_TM_std_trans_design.append(DBR_TM_trans[n_pair_index, wavelength_design_index, 1])
+    DBR_TM_std_reflect_design.append(DBR_TM_reflect[n_pair_index, wavelength_design_index, 1])
+    
+    # Plot spectral reflectance for this design
+    show_spectral_reflectance = True
+    if show_spectral_reflectance:
+        plt.figure()
+        plt.plot(wavelengths*1e9, DBR_TE_reflect[n_pair_index, :],'k',label='R')
+        plt.plot(wavelengths*1e9, DBR_TE_trans[n_pair_index, :],'#87aadeff',label='T')
+        absorpt = 1 - DBR_TE_reflect[n_pair_index, :, 0] - DBR_TE_trans[n_pair_index, :, 0]
+        plt.plot(wavelengths*1e9, absorpt,'#ff5555ff',label='A')
+        plt.axvline(x = wavelength_design*1e9, linestyle = '--', color = 'k')
+        plt.axhline(y = target_reflectance_bottom, linestyle = '--', color = 'k')
+        plt.xlim(wavelengths[0]*1e9, wavelengths[-1]*1e9)
+        plt.ylim(0, 1)
+        plt.grid()
+        plt.xlabel('Wavelegnth (nm)')
+        plt.ylabel('Reflectance')
+        plt.title('Bottom DBR mirror')
+        plt.show()
+    
+    # Print this reflectance
+    this_DBR_TE_reflect = DBR_TE_mean_reflect_design[n_pair_index]
+    print(f"N layers: {n_pair} | Reflectance: {this_DBR_TE_reflect}")
+    # Next N
+    n_pair_index += 1
+    
